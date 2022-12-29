67a66320868aa8eae0b66e159c088f5bd134384d
Olivia Persson <olivia.persson@forefront.se>
2022-12-29 14:13:47 +0100

    Add card content with styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,28 @@
 import "./Card.css";
+import katieImage from "../images/katie-zaferes.png";
+import star from "../images/star.png";
 
 const Card = () => {
-  return <h1>Card</h1>;
+  return (
+    <div className="card-container">
+      <img className="card-image" src={katieImage} alt="Katie Zaferes" />
+      <div className="card-text-container">
+        <div className="rating-text-container">
+          <img className="star" src={star} alt="star" />
+          <p className="rating">5.0</p>
+          <p className="rating-amount">(6)</p>
+          <div className="ellipse"></div>
+          <p className="country">USA</p>
+        </div>
+        <p className="experience-description">
+          Life lessons with Katie Zaferes
+        </p>
+        <p className="cost">
+          <strong>From $136 </strong>/ person
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
