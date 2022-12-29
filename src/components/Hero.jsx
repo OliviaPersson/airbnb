9218d74f70454c191b4b72a1f1fdeff8ee2652a8
Olivia Persson <olivia.persson@forefront.se>
2022-12-29 12:19:29 +0100

    Add content to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,23 @@
 import "./Hero.css";
+import imageCollection from "../images/image-collection.png";
 
 const Hero = () => {
-  return <h1>Test</h1>;
+  return (
+    <div className="hero-container">
+      <img
+        className="image-collection"
+        src={imageCollection}
+        alt="Collection of various images"
+      />
+      <div className="text-content-container">
+        <h1 className="text-content-label">Online Experiences</h1>
+        <p className="text-content">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
